a9fccfc8eeb4db77ffe5ae3e4a3cc9d334c27346
davidleonschmidt <davidleonschmidt@mensching>
2022-04-11 16:53:07 +0200

    output/input start

--- habit tracker/habit tracker/Program.cs
+++ habit tracker/habit tracker/Program.cs
@@ -5,37 +5,84 @@ namespace Variables
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
             const string connectionString = @"Data Source=.\\habit-Tracker.db";
-
-            /*Creating a connection passing the connection string as an argument
-            This will create the database for you, there's no need to manually create it.
-            And no need to use File.Create().*/
-            using (var connection = new SqliteConnection(connectionString))
+            var con = new SqliteConnection(connectionString);
+            using (con)
             {
-                var tableCmd = connection.CreateCommand();
-                //Creating the command that will be sent to the database
-                using (tableCmd);
+                var tableCmd = con.CreateCommand();
+                using (tableCmd) 
                 {
-                    //Declaring what is that command (in SQL syntax)
                     tableCmd.CommandText =
-                        @"CREATE TABLE IF NOT EXISTS yourHabit (
+                       @"CREATE TABLE IF NOT EXISTS yourHabit (
                             Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            Date TEXT,
+                           Date TEXT,
                             Quantity INTEGER
                             )";
-                    connection.Open();
-                    // Executing the command, which isn't a query, it's not asking to return data from the database.
+                    con.Open();
                     tableCmd.ExecuteNonQuery();
-                    connection.Close();
-
+                    con.Close();
                 }
-                // We don't need to close the connection or the command. The 'using statement' does that for us.
+            }
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine(" ");
+            Console.WriteLine("Type 1 to Close Application");
+            Console.WriteLine("Type 2 to view All Records");
+            Console.WriteLine("Type 3 to Insert Record");
+            Console.WriteLine("Type 4 to Delete Record");
+            Console.WriteLine("Type 5 to Update Record");
+            byte userInput = 0;
+            while (userInput == 0)
+                try
+                {
+                    userInput = Convert.ToByte(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Bitte eine Zahl eingeben");
+                }
+            if (userInput == 1) 
+            {
+                System.Environment.Exit(0);
+            }
+            
+            Console.WriteLine(userInput);
+
+
+            static void request() 
+            {
+
             }
 
-            /* Once we check if the database exists and create it (or not),
-            we will call the next method, which will handle the user's input.*/
-            //GetUserInput();
+            ///*Creating a connection passing the connection string as an argument
+            //This will create the database for you, there's no need to manually create it.
+            //And no need to use File.Create().*/
+            //using (var connection = new SqliteConnection(connectionString))
+            //{
+            //    var tableCmd = connection.CreateCommand();
+            //    //Creating the command that will be sent to the database
+            //    using (tableCmd);
+            //    {
+            //        //Declaring what is that command (in SQL syntax)
+            //        tableCmd.CommandText =
+            //            @"CREATE TABLE IF NOT EXISTS yourHabit (
+            //                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            //                Date TEXT,
+            //                Quantity INTEGER
+            //                )";
+            //        connection.Open();
+            //        // Executing the command, which isn't a query, it's not asking to return data from the database.
+            //        tableCmd.ExecuteNonQuery();
+            //        connection.Close();
+
+            //    }
+            //    // We don't need to close the connection or the command. The 'using statement' does that for us.
+            //}
+
+            ///* Once we check if the database exists and create it (or not),
+            //we will call the next method, which will handle the user's input.*/
+            ////GetUserInput();
 
         }
     }
