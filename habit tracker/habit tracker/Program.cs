dda34ce2cf3071adf2e0cca9f683b9dece36958b
davidleonschmidt <davidleonschmidt@mensching>
2022-04-11 16:04:48 +0200

    include sqlite and create first db object

--- habit tracker/habit tracker/Program.cs
+++ habit tracker/habit tracker/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.Sqlite;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = @"Data Source=.\\habit-Tracker.db";
+
+            /*Creating a connection passing the connection string as an argument
+            This will create the database for you, there's no need to manually create it.
+            And no need to use File.Create().*/
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                var tableCmd = connection.CreateCommand();
+                //Creating the command that will be sent to the database
+                using (tableCmd);
+                {
+                    //Declaring what is that command (in SQL syntax)
+                    tableCmd.CommandText =
+                        @"CREATE TABLE IF NOT EXISTS yourHabit (
+                            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            Date TEXT,
+                            Quantity INTEGER
+                            )";
+                    connection.Open();
+                    // Executing the command, which isn't a query, it's not asking to return data from the database.
+                    tableCmd.ExecuteNonQuery();
+                    connection.Close();
+
+                }
+                // We don't need to close the connection or the command. The 'using statement' does that for us.
+            }
+
+            /* Once we check if the database exists and create it (or not),
+            we will call the next method, which will handle the user's input.*/
+            //GetUserInput();
+
+        }
+    }
+}
+
+
