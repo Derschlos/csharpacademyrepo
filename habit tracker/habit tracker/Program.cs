7c2aced0e91663e0bbe8eca171f88eb84c1b7768
Home <David_leon_schmidt@live.de>
2022-04-17 13:59:43 +0200

    finished insert delete next

--- habit tracker/habit tracker/Program.cs
+++ habit tracker/habit tracker/Program.cs
@@ -71,35 +71,37 @@ namespace Variables
                 }
             }
         }
+        static String checkRegex(String pattern, String uInput)
+        {
+            Regex rgx = new Regex(pattern);
+            MatchCollection matches = rgx.Matches(uInput);
+            if (matches.Count > 0)
+            {
+                return matches[0].Value;
 
+            }
+            else
+            {
+                return null;
+            }
+        }
+        static int maxId()
+        {
+            String inputString = @"SELECT MAX(ID) FROM yourHabit";
+            var outputObject = executeSql(inputString, "r");
+            return Convert.ToInt32(outputObject[0]);
+        }
         static String getDate()   
         {
-            static String checkDayMonthYear(String pattern, String uInput) 
-                {
-                    Regex rgx = new Regex(pattern);
-                    MatchCollection matches = rgx.Matches(uInput);
-                    if (matches.Count > 0)
-                    {
-                        return matches[0].Value;
-
-                    }
-                    else
-                    {
-                        return null;
-                    }
-                }
-            int i = 0;
-            //Console.WriteLine("in 3");
-            String inputString = @"SELECT MAX(ID) FROM yourHabit";
-            //inputString = @"SELECT * FROM yourHabit WHERE ID = 1";
-            var outputObject = executeSql(inputString, "r");
-            var index = Convert.ToString(Convert.ToInt32(outputObject[0]) + 1);
+            var index = Convert.ToString(maxId() + 1);
             Console.WriteLine("Inserting new entry with ID: " + index);
+
             String date = DateTime.Now.ToShortDateString();
             Console.WriteLine("Current Date is: " + date);
             Console.WriteLine("Do you want to change the Date? (y/n)");
             string changeDate = Console.ReadLine();
             string breakChain = "break";
+           
             while (changeDate == "y")
             {
                 Console.WriteLine("Please write the Date as dd.mm.yyyy:");
@@ -115,14 +117,11 @@ namespace Variables
                 if (date.Length == 10)
                 {
                     string pattern = @"\d\d(?=[.,/ \-]+\d\d[.,/ \\-]+\d{4})";
-                    day = checkDayMonthYear(pattern, date);
+                    day = checkRegex(pattern, date);
                     pattern = @"(?<=[.,/\-])\d\d(?=[.,/\-]+\d{4})";
-                    month = checkDayMonthYear(pattern, date);
+                    month = checkRegex(pattern, date);
                     pattern = @"(?<=[.,/\\-])\d{4}";
-                    year = checkDayMonthYear(pattern, date);
-                    Console.WriteLine(day);
-                    Console.WriteLine(month);
-                    Console.WriteLine(year);    
+                    year = checkRegex(pattern, date);   
                     date = $"{day}.{month}.{year}";
                     if (Convert.ToInt32(day) <= 31 && Convert.ToInt32(month) <= 12)
                     {
@@ -131,7 +130,23 @@ namespace Variables
                     Console.WriteLine("Faulty input. Type (break) if you want to stop Input and use today as date.");
                 }
             }
-            return date
+            Console.Clear();
+            return date;
+
+        }
+
+        static string getQuant()
+        {
+            Console.WriteLine("Please Input the Quantity of the task");
+            string uInput = Console.ReadLine();
+            string quant = checkRegex(@"\d+", uInput);
+            if (quant == null)
+            {
+                Console.WriteLine("Ivalid Input");
+                return getQuant();
+            }
+            Console.Clear();
+            return quant;
 
         }
 
@@ -164,16 +179,13 @@ namespace Variables
                     case 2:
                         //Insert Record
                         Console.Clear();
-                        var insertDate = getDate();
-                        Console.WriteLine(insertDate);
-
-
-
-                        //inputString = String.Format(@"INSERT INTO yourHabit (ID, Date, Quantity)
-                        //VALUES ('{0}', '03.03.2022', '{1}')", index , i + 1);
-                        //insert();
-                        
-                        //outputObject = executeSql(inputString, "w");
+                        var date = getDate();
+                        var quan = getQuant();
+                        var index = Convert.ToString(maxId() + 1);
+                        inputString = @$"INSERT INTO yourHabit (ID, Date, Quantity)
+                                         VALUES ('{index}', '{date}', '{quan}')";
+                        Console.WriteLine($"Inserting {quan} of habit on the {date} at index {index}");
+                        outputObject = executeSql(inputString, "w");
                         break;
                     case 3:
                         //Delete Record
