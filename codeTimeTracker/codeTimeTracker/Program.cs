46eff08f568be22eed2685591d90f52370780445
davidleonschmidt <davidleonschmidt@mensching>
2022-04-19 16:11:13 +0200

    CodeTimeTracker first commit

--- codeTimeTracker/codeTimeTracker/Program.cs
+++ codeTimeTracker/codeTimeTracker/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Configuration;
+using System.Collections.Specialized;
+using Microsoft.Data.Sqlite;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+
+
+string sAtt = ConfigurationManager.AppSettings.Get("databasePath");
+Console.WriteLine("between");
+Console.WriteLine(sAtt);
+Console.WriteLine("these");
