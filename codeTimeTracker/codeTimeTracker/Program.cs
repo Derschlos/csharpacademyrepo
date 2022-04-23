01f85bc8a6cee80628fea6ae62fe6b3ea85c4c98
Derschlos <None>
2022-04-23 23:14:59 +0200

    consoleTableExt first input

--- codeTimeTracker/codeTimeTracker/Program.cs
+++ codeTimeTracker/codeTimeTracker/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Data.Sqlite;
 using System.Globalization;
 using System.Text.RegularExpressions;
 using codeTimeTracker;
+using ConsoleTableExt;
 
 //UserInput uIn = new UserInput();
 SqlDriver sql = new SqlDriver();
@@ -43,8 +44,50 @@ List<string> manualMenu = new List<string>()
 };
 int userInput = 800;
 
-CodingSession currentSession = new CodingSession();
+var tableData = new List<List<object>>
+{
+    new List<object>{ "Sakura Yamamoto", "Support Engineer", "London", 46},
+    new List<object>{ "Serge Baldwin", "Data Coordinator", "San Francisco", 28, "something else" },
+    new List<object>{ "Shad Decker", "Regional Director", "Edinburgh"},
+};
 
+ConsoleTableBuilder.From(tableData)
+                .WithTextAlignment(new Dictionary<int, TextAligntment> {
+                    { 0, TextAligntment.Center },
+                    { 1, TextAligntment.Center },
+                    { 3, TextAligntment.Center },
+                    { 4, TextAligntment.Center }
+                })
+                .WithMinLength(new Dictionary<int, int> {
+                    { 1, 35 },
+                    { 3, 10 },
+                    { 4, 30 },
+                })
+                //.WithFormatter(2, (text) => {
+                //    char[] chars = text.ToCharArray();
+                //    Array.Reverse(chars);
+                //    return new String(chars);
+                //})
+                .WithTitle("Hello, everyone! This is the LONGEST TEXT EVER! I was inspired by the various other 'longest texts ever' on the internet, and I wanted to make my own. So here it is!".ToUpper(), ConsoleColor.Yellow, ConsoleColor.DarkMagenta)
+                .WithCharMapDefinition(CharMapDefinition.FrameDoublePipDefinition)
+                .WithFormatter(3, (text) => {
+                    if (string.IsNullOrEmpty(text) || text.Trim().Length == 0)
+                    {
+                        return "0 $";
+                    }
+                    else
+                    {
+                        return text + " $";
+                    }
+                })
+                .WithColumnFormatter(3, (text) => "#")
+                .ExportAndWriteLine();
+
+//CodingSession currentSession = new CodingSession();
+
+//ConsoleTableBuilder
+//    .From(tableData)
+//    .ExportAndWriteLine();
 while (userInput != 0)
 {
     userInput = UserInput.menu(mainMenu);
